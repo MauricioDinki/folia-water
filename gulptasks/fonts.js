ea2bc950bb3a9f46ad8b2985209e5a6e4b8e20ae
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2019-03-13 16:30:00 -0600

    Update 2019-03-13T22:29:41.864Z

--- gulptasks/fonts.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp'),
-  paths = require('../gulpconfig').paths,
-  argv = require('yargs').argv,
-  production = argv.production;
-
-gulp.task('copy:fonts', function () {
-  return gulp.src(paths.getSrc('fonts'))
-    .pipe(gulp.dest(paths.getCompiled(production, 'fonts')));
-});
--- gulptasks/ghpages.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-  paths = require('../gulpconfig').paths,
-  ghPages = require('gulp-gh-pages');
-
-gulp.task('deploy', function() {
-  return gulp.src(paths.getCompiled(true, 'all'))
-    .pipe(ghPages());
-});
