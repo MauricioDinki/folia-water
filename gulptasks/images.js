988f30040ac38370951cdcbd4df82014a8cc66b2
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2019-03-15 05:24:25 -0600

    Fixed images on sultasks

--- gulptasks/images.js
+++ gulptasks/images.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
   config = require('../gulpconfig'),
+  paths = require('../gulpconfig').paths,
   imagens = require('gulp-image'),
   gulpIf = require('gulp-if'),
   argv = require('yargs').argv,
@@ -9,8 +10,9 @@ gulp.task('copy:images', function () {
   //var baseDir = production ? config.paths.dist : config.paths.build;
   //// TODO: Modify source to get source from gulpconfig
   gulp.src(['./src/img/**/*.{gif,png,jpg,svg}'])
-  .pipe(imagens())
-  .pipe(gulp.dest('./build/img'));
+  // .pipe(imagens())
+  // .pipe(gulp.dest('./build/img'))
+  .pipe(gulp.dest(paths.getCompiled(production, 'img')));
 
   //gulp.src(config.paths.src.favicon)
   // .pipe(gulp.dest(baseDir.root));
